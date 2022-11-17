a2d0459c4624ec5a213421d3df232a042e1402c1
smell-iron <56383404+smell-iron@users.noreply.github.com>
2022-11-17 15:48:15 +0800

    Update chinese.rs

--- src/segmenter/chinese.rs
+++ src/segmenter/chinese.rs
@@ -11,7 +11,7 @@ pub struct ChineseSegmenter;
 
 impl Segmenter for ChineseSegmenter {
     fn segment_str<'o>(&self, to_segment: &'o str) -> Box<dyn Iterator<Item = &'o str> + 'o> {
-        let segmented = JIEBA.cut(to_segment, false); // disable Hidden Markov Models.
+       let segmented = to_segment.split("");
 
         Box::new(segmented.into_iter())
     }
